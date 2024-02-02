137e216541ed75fcb51ea67a18716120d873b219
GabrielSouza2006 <bielss1609@gmail.com>
2024-02-02 19:14:58 -0300

    Home

---
+++ home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/text.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Home</title>
+</head>
+
+<body>
+
+    <main>
+        <section class="container-fluid sobre">
+            <div class="container">
+                <h1 id="title-01">INK STUDIO</h1>
+            </div>
+
+            <div class="container text-sobre">
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col">
+                            <div id="img-01">
+                                <img src="images/LogoINK.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <article id="sobre">
+                                <h3>Quem somos?</h3>
+                                <p>
+                                    Somos uma empresa de tatuagem e piercing, realizamos qualquer tipo de serviço com auxilio de
+                                    nossos
+                                    profissionais altamente qualificados e preparados. Visamos manter o máximo de conforto aos
+                                    nossos
+                                    clientes por isso criamos um site onde os nossos clientes possam manter contato com o tatuador
+                                    sem
+                                    precisar ir na loja física, além de poder realizar compras de produtos, fazer orçamentos e
+                                    agendar
+                                    horários tudo isso online.</p>
+                                </p>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </section>
+
+    </main>
+</body>
+
+</html>
