b2d8ba6dc9694c1eb094442f5aa62a2a5e4d406c
GabrielSouza2006 <bielss1609@gmail.com>
2024-02-04 15:03:28 -0300

    Loja home

--- home.php
+++ home.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="css/text.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <title>Home</title>
 </head>
@@ -45,12 +46,43 @@
                         </div>
                     </div>
                 </div>
-
-
-
             </div>
         </section>
 
+        <section class="container-fluid home-loja">
+            <div class="container">
+                <h1 id="title-01">SERVIÇOS</h1> <br>
+                <h2 id="sub-title-01">Loja</h2>
+            </div>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <img src="images/CamisaINK.jpg" alt="" id="prod-02">
+                        <p id="prod-desc">Camisa Ink Studio</p>
+                        <p id="price">R$49,99</p>
+                        <img src="images/5estrelas.png" alt="" id="stars">
+                    </div>
+                    <div class="col">
+                        <img src="images/Cordao.jpg" alt="" id="prod-01">
+                        <p id="prod-desc">Cordão</p>
+                        <p id="price">R$9,99</p>
+                        <img src="images/5estrelas.png" alt="" id="stars">
+                    </div>
+                    <div class="col">
+                        <img src="images/Anel.jpg" alt="" id="prod-01">
+                        <p id="prod-desc">Anel</p>
+                        <p id="price">R$15,99</p>
+                        <img src="images/5estrelas.png" alt="" id="stars">
+                    </div>
+                    <div class="col">
+                        <img src="images/Pulsera.jfif" alt="" id="prod-01">
+                        <p id="prod-desc">Camisa Ink Studio</p>
+                        <p id="price">R$12,99</p>
+                        <img src="images/5estrelas.png" alt="" id="stars">
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 </body>
 
